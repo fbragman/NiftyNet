edited to fix some bugs and also allow for global average pooling

niftynet/network/vgg_mt.py
@@ -15,13 +15,13 @@ class MT1_VGG16Net(BaseNet):
     Multi-task VGG net
     Sharing done by:
         1) split just before last FC unit that is fed into loss function
+        2) we use global average pooling instead of multiple FC units
             e.g. fc (1x1) --> l2 loss (age regression)
                  fc (1x2) --> binary softmax cross entropy (gender - 0/1)
     """
 
     def __init__(self,
-                 task1_classes,
-                 task2_classes,
+                 num_classes,
                  w_initializer=None,
                  w_regularizer=None,
                  b_initializer=None,
@@ -30,8 +30,7 @@ class MT1_VGG16Net(BaseNet):
                  name='MT1_VGG16Net'):
 
         super(MT1_VGG16Net, self).__init__(
-            task1_classes=task1_classes,
-            task2_classes=task2_classes,
+            num_classes=num_classes,
             w_initializer=w_initializer,
             w_regularizer=w_regularizer,
             b_initializer=b_initializer,
@@ -49,15 +48,10 @@ class MT1_VGG16Net(BaseNet):
             {'name': 'layer_4', 'n_features': 512*2, 'kernel_size': 3, 'repeat': 3},
             {'name': 'maxpool_4'},
             {'name': 'layer_5', 'n_features': 512*2, 'kernel_size': 3, 'repeat': 3},
-            {'name': 'maxpool_5'},
-            {'name': 'fc_1', 'n_features': 4096*2},
-            {'name': 'fc_2', 'n_features': 4096*2}]
+            {'name': 'gap'}]
 
-        self.task1_layers = [
-            {'name': 'task_1_out', 'n_features': task1_classes}]
-
-        self.task2_layers = [
-            {'name': 'task_2_out', 'n_features': task2_classes}]
+        self.task1_layers = {'name': 'task_1_out', 'n_features': self.num_classes[0]}
+        self.task2_layers = {'name': 'task_2_out', 'n_features': self.num_classes[1]}
 
     def layer_op(self, images, is_training=True, layer_id=-1, **unused_kwargs):
 
@@ -119,17 +113,6 @@ class MT1_VGG16Net(BaseNet):
                 layer_instances.append((conv_layer, flow))
                 repeat_conv = repeat_conv - 1
 
-            # last layer
-            if layer_iter == len(self.layers) - 1:
-                fc_layer = FullyConnectedLayer(
-                    n_output_chns=layer['n_features'],
-                    w_initializer=self.initializers['w'],
-                    w_regularizer=self.regularizers['w'],
-                )
-                flow = fc_layer(flow)
-                layer_instances.append((fc_layer, flow))
-                continue
-
             # all other
             if layer_type == 'maxpool':
                 downsample_layer = DownSampleLayer(
@@ -138,6 +121,13 @@ class MT1_VGG16Net(BaseNet):
                     stride=2)
                 flow = downsample_layer(flow)
                 layer_instances.append((downsample_layer, flow))
+
+            elif layer_type == 'gap':
+                with tf.name_scope('global_average_pool'):
+                    flow = tf.reduce_mean(flow, axis=[1, 2])
+                    # dummy layer
+                    dmy = DownSampleLayer(func='AVG')
+                    layer_instances.append((dmy, flow))
 
             elif layer_type == 'layer':
 