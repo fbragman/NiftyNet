8deed3cd32bbbfb07d61e25b0aeafd3c48f666ee
fbragman <f.bragman@ucl.ac.uk>
2018-10-24 13:41:43 +0100

    runner for model evaluation to be put in each experiment directory where config file is

--- paper_experiments/baseline_1_multi_task/run_model_evaluation.py
+++ paper_experiments/baseline_1_multi_task/run_model_evaluation.py
@@ -2,6 +2,7 @@ from paper_experiments import evaluation
 import configparser
 import os
 import inspect
+import pandas as pd
 from pathlib import Path
 
 # get cwd
@@ -23,6 +24,19 @@ def get_files(task):
             for f in os.listdir(results_path) if task in f]
 
 
+def create_csv_and_save(results, name):
+    """
+    Csv and save in root
+    :param result:
+    :param name:
+    :return:
+    """
+    df = pd.DataFrame(data=results)
+    fname = name + '.csv'
+    fname = os.path.join(root, fname)
+    df.to_csv(fname, sep='\t', encoding='utf-8')
+
+
 def perform_experiment_evaluation(task):
     """
     Evaluate results
@@ -35,6 +49,15 @@ def perform_experiment_evaluation(task):
     if task is 'regression':
         # calculate mean absolute error across dataset and across binned ages
         total_error, binned_error = evaluation.calculate_mae(list_of_files)
+        results = {'mae': total_error, 'binned_mae': binned_error}
+        create_csv_and_save(results, 'age_error')
+
+    elif task is 'classification':
+        # calculate classification stats: accuracy, precision and recall
+        total_stats, binned_stats, bins = evaluation.calculate_classification(list_of_files)
+        create_csv_and_save(total_stats, 'classification_stats')
+        create_csv_and_save(binned_stats, 'classification_age_binned_stats')
+        create_csv_and_save(bins, 'bins')
 
 
 if __name__ == '__main__':
