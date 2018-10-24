9e9c24cbca182d7b6da02533419189cb3e685564
fbragman <f.bragman@ucl.ac.uk>
2018-10-24 16:34:42 +0100

    ability to parse Gumbel Softmax options to network through net_args

--- niftynet/layer/annealing.py
+++ niftynet/layer/annealing.py
@@ -1,15 +1,17 @@
 import numpy as np
 
 
-def gumbel_softmax_decay(current_iter, r):
+def gumbel_softmax_decay(current_iter, r, max_temp, min_temp):
     """
     Annealing schedule used in Gumbel-Softmax paper
     Jang et al. Categorical Reparameterization with Gumbel Softmax ICLR 2017
     :param current_iter: current training iteration
     :param r: hyperparameter, suggested range {1e-5, 1e-4}
+    :param max_temp: initial tau
+    :param min_temp: minimum tau
     :return: temperature
     """
-    return np.max(0.5, np.exp(-r * current_iter))
+    return np.maximum(min_temp, max_temp * np.exp(-r * current_iter))
 
 
 def exponential_decay(current_iter, initial_lr, k):
