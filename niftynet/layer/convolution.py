993b39396de2cf7382cbc772c30181cde90f59c1
fbragman <f.bragman@ucl.ac.uk>
2019-02-05 11:24:44 +0000

    adding name_scope and setting GS to always soft, even at trainin=False

--- niftynet/layer/convolution.py
+++ niftynet/layer/convolution.py
@@ -1138,10 +1138,11 @@ class LearnedCategoricalGroupConvolutionalLayer(TrainableLayer):
                     #cat_dist = Categorical(dirichlet_p)
                     #cat_mask = cat_dist()
                     #cat_mask_unstacked = tf.unstack(cat_mask, axis=1)
-                    cat_dist = GumbelSoftmax(dirichlet_p, tau)
-                    # Sample from mask - [N by 3] either one-hot (use_hardcat=True) or soft (use_hardcat=False)
-                    cat_mask = cat_dist(hard=self.use_hardcat, is_training=is_training)
-                    cat_mask_unstacked = tf.unstack(cat_mask, axis=1)
+                    with tf.variable_scope('categorical_sampling'):
+                        cat_dist = GumbelSoftmax(dirichlet_p, tau)
+                        # Sample from mask - [N by 3] either one-hot (use_hardcat=True) or soft (use_hardcat=False)
+                        cat_mask = cat_dist(hard=self.use_hardcat, is_training=True)
+                        cat_mask_unstacked = tf.unstack(cat_mask, axis=1)
 
             else:
                 # soft weighting using p
