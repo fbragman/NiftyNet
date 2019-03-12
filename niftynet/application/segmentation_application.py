1a089c01738b482b7a7f9a6444ca7f5f5b1299d9
fbragman <f.bragman@ucl.ac.uk>
2019-03-12 19:20:46 +0000

    minor edits

--- niftynet/application/segmentation_application.py
+++ niftynet/application/segmentation_application.py
@@ -359,11 +359,9 @@ class SegmentationApplication(BaseApplication):
                                      loss_type='Dice')
 
             reporting_string = 'task_1_dice'
-
             data_loss_task_1 = dice_loss(
                 prediction=net_out[0],
                 ground_truth=data_dict['output_1'])
-
             outputs_collector.add_to_collection(
                 var=data_loss_task_1, name=reporting_string,
                 average_over_devices=False, collection=CONSOLE)
